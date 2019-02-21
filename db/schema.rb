4db1fc983c649c88c43e639a8bf60057c805becf
Maricela <ximena84@hotmail.com>
2019-02-20 20:56:45 -0600

    products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_024344) do
+ActiveRecord::Schema.define(version: 2019_02_21_025418) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "vendor"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "vendors", force: :cascade do |t|
     t.string "vendor"
